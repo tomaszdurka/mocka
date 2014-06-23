18325f6fb0f2cde825761d3a128a6d66857d162b
tomaszdurka <tomasz@durka.pl>
2014-06-23 03:46:38 +0200

    Add test or Mocka\ClassTrait->mockInterface

--- tests/source/Mocka/ClassTraitTest.php
+++ tests/source/Mocka/ClassTraitTest.php
@@ -6,6 +6,18 @@ use \Mocka\Mocka;
 
 class ClassTraitTest extends \PHPUnit_Framework_TestCase {
 
+    public function testMockClass() {
+        $mocka = new Mocka();
+        $mockClass = $mocka->mockClass('\MockaMocks\AbstractClass');
+        $this->assertInstanceOf('\\Mocka\\ClassMock', $mockClass);
+    }
+
+    public function testMockInterface() {
+        $mocka = new Mocka();
+        $mockClass = $mocka->mockInterface('\MockaMocks\InterfaceMock');
+        $this->assertInstanceOf('\\Mocka\\ClassMock', $mockClass);
+    }
+
     public function testCallMockedMethod() {
         $mocka = new Mocka();
         $mockClass = $mocka->mockClass('\MockaMocks\AbstractClass');
